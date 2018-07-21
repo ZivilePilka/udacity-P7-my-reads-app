97059071875a8390c0aa2bc4eff9a99a1cf48954
ZivilePilka <zivile_pilkauskaite@yahoo.com>
2018-07-21 18:36:52 +0300

    Add icon to go back from search page

--- src/Search.js
+++ src/Search.js
@@ -6,7 +6,7 @@ class Search extends Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link to="/">Close</Link>
+          <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
