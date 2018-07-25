84292a6b73cb3587c913110af0aeb0ed3595f402
ZivilePilka <zivile_pilkauskaite@yahoo.com>
2018-07-25 18:22:38 +0300

    Refactor Book component to Stateless functional component

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react'
 
-class Book extends Component {
-  render () {
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover"
-               style={{ width: 128, height: 193,
-               backgroundImage: this.props.book.imageLinks ? `url("${this.props.book.imageLinks.thumbnail}")` : '' }}>
-          </div>
-          <div className="book-shelf-changer">
-            <select value={this.props.book.shelf}
-              onChange={(event) => this.props.updateShelf(
-                this.props.book, event.target.value
-              )}
-            >
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+function Book (props) {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover"
+             style={{ width: 128, height: 193,
+             backgroundImage: props.book.imageLinks ? `url("${props.book.imageLinks.thumbnail}")` : '' }}>
         </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors}</div>
+        <div className="book-shelf-changer">
+          <select value={props.book.shelf}
+            onChange={(event) => props.updateShelf(
+              props.book, event.target.value
+            )}
+          >
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
       </div>
-    )
-  }
+      <div className="book-title">{props.book.title}</div>
+      <div className="book-authors">{props.book.authors}</div>
+    </div>
+  )
 }
 
 export default Book
