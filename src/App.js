718f6998226b6d882fb57b3a6729c4471025ec0f
ZivilePilka <zivile_pilkauskaite@yahoo.com>
2018-07-27 14:18:11 +0300

    Fix react console warnings

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import BookList from './BookList'
 import Search from './Search'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import './App.css'
 
 class BooksApp extends React.Component {
