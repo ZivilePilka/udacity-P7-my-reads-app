3b6d9f9cf722de5cd6ede796f8f69e14e71c65c4
ZivilePilka <zivile_pilkauskaite@yahoo.com>
2018-07-27 14:05:48 +0300

    Refactor shelves in BookList.js to more efficient code

--- src/BookList.js
+++ src/BookList.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
 
 function BookList(props) {
+  const shelves = ["currentlyReading", "wantToRead", "read"]
+  const shelfTitle = ["Currently reading", "Want to read", "Read"]
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -10,65 +12,33 @@ function BookList(props) {
         </div>
         <div className="list-books-content">
         <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {props.books
-                  .filter(book => book.shelf === "currentlyReading")
-                  .map(book => (
-                  <li key={book.id}>
-                    <Book
-                      book={book}
-                      updateShelf={props.updateShelf}
-                    />
-                  </li>
-                  ))
-                }
-              </ol>
-            </div>
-          </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want to Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {props.books
-                  .filter(book => book.shelf === "wantToRead")
-                  .map(book => (
-                  <li key={book.id}>
-                    <Book
-                      book={book}
-                      updateShelf={props.updateShelf}
-                    />
-                  </li>
-                  ))
-                }
-              </ol>
-            </div>
-          </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {props.books
-                  .filter(book => book.shelf === "read")
-                  .map(book => (
-                  <li key={book.id}>
-                    <Book
-                      book={book}
-                      updateShelf={props.updateShelf}
-                    />
-                  </li>
-                  ))
-                }
-              </ol>
-            </div>
-          </div>
+          {shelves.map((shelf, index) => {
+            return(
+              <div key={shelves[index]} className="bookshelf">
+                <h2 className="bookshelf-title">{shelfTitle[index]}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {props.books
+                      .filter(book => book.shelf === shelf)
+                      .map(book => (
+                      <li key={book.id}>
+                        <Book
+                          book={book}
+                          updateShelf={props.updateShelf}
+                        />
+                      </li>
+                      ))
+                    }
+                  </ol>
+                </div>
+              </div>
+            )
+          })}
         </div>
         </div>
-          <div className="open-search">
-            <Link to="/search">Add a book</Link>
-          </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
       </div>
     )
 }
